c874a3a1e2739d141327bc84d3050d581a553c44
Ashika AK <ashikaak101@gmail.com>
2025-08-04 14:02:39 +0530

    Add banner image slider and update assets

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,14 +2,19 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../pages/context/AuthContext";
 
-
-
 const ProtectedRoute = ({ children, allowedRoles }) => {
   const { isAuthenticated, user } = useAuth();
 
-  if (!isAuthenticated) return <Navigate to="/login" />;
+  // If AuthContext says not authenticated, fallback to localStorage
+  const token = localStorage.getItem("token");
+  const localUser = JSON.parse(localStorage.getItem("user"));
 
-  if (allowedRoles && !allowedRoles.includes(user?.role)) {
+  const effectiveIsAuthenticated = isAuthenticated || !!token;
+  const effectiveUser = user || localUser;
+
+  if (!effectiveIsAuthenticated) return <Navigate to="/login" />;
+
+  if (allowedRoles && !allowedRoles.includes(effectiveUser?.role)) {
     return <Navigate to="/unauthorized" />;
   }
 
